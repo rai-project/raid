d846d61ffd0d023e3f0ae82470d8fe8de9db0277
Abdul Dakkak <abduld@wolfram.com>
2018-06-20 15:36:33 -0500

    Bump version: 0.2.17 → 0.2.18

--- version.go
+++ version.go
@@ -4,7 +4,7 @@ import "github.com/rai-project/config"
 
 var (
 	// These fields are populated by govvv
-	Version    = "0.2.17"
+	Version    = "0.2.18"
 	BuildDate  string
 	GitCommit  string
 	GitBranch  string
